bcd8e198972f02db78d873a496fc121aea11694f
Jason Gerard <jasongerard321@gmail.com>
2023-01-21 14:29:01 -0500

    Base proj setup

---
+++ index.js
@@ -0,0 +1,22 @@
+const { Client } = require("@hashgraph/sdk");
+require("dotenv").config();
+
+async function main() {
+
+    //Grab your Hedera testnet account ID and private key from your .env file
+    const myAccountId = process.env.MY_ACCOUNT_ID;
+    const myPrivateKey = process.env.MY_PRIVATE_KEY;
+
+    // If we weren't able to grab it, we should throw a new error
+    if (!myAccountId || !myPrivateKey) {
+        throw new Error("Environment variables MY_ACCOUNT_ID and MY_PRIVATE_KEY must be present");
+    }
+
+    // Create our connection to the Hedera network
+    // The Hedera JS SDK makes this really easy!
+    const client = Client.forTestnet();
+
+    client.setOperator(myAccountId, myPrivateKey);
+}
+
+main().catch(err => console.error(err));
