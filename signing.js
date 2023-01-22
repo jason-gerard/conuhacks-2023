6b1c1eaa60547f1aebea020a98cf73003844af3c
Alexandru Bara <alexandrubara2000@gmail.com>
2023-01-22 01:50:01 -0500

    added async

--- signing.js
+++ signing.js
@@ -21,25 +21,26 @@ async function signAndEncrypt(content){
     // const signature = sign.toString('base64');
 
     // encrypt the data
-    const encrypted = CryptoJS.AES.encrypt(content, secretKey);
+    const encrypted = await CryptoJS.AES.encrypt(content, secretKey);
     return {"publicKey": publicKey, "Signature": signature, "encryptedData": encrypted}
 
 }
 
-function decryptAndValidate(publicKey, sign, encrypted){
-    const decryptedBytes = CryptoJS.AES.decrypt(encrypted, secretKey)
+async function decryptAndValidate(publicKey, sign, encrypted){
+    const decryptedBytes = await CryptoJS.AES.decrypt(encrypted, secretKey)
     const plaintext = decryptedBytes.toString(CryptoJS.enc.Utf8);
     const isValid = crypto.verify("SHA256",Buffer.from(plaintext), publicKey, sign)
-
     return {"data": plaintext, "valid": isValid}
 }
 
-
 signAndEncrypt("dsfdsfsdfsdfdsdfsfweeeeeee").then(
     (res) => {
-    const {data, isValid} = decryptAndValidate(res.publicKey, res.Signature, res.encryptedData);
-    console.log(data);
-    console.log(isValid);
+        decryptAndValidate(res.publicKey, res.Signature, res.encryptedData).then(
+            (res) => {
+                console.log(res.data);
+                console.log(res.valid);
+            }
+        )
     }
 )
 
