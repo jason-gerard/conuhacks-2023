6350691a210df8791cf2e92379353828ffa5c316
Alexandru Bara <alexandrubara2000@gmail.com>
2023-01-22 01:36:29 -0500

    Added signing and encryption

--- signing.js
+++ signing.js
@@ -1,9 +1,12 @@
 const crypto = require('crypto');
 const buffer = require('buffer');
 const {sign} = require("crypto");
+const CryptoJS = require("crypto-js");
+
+const secretKey="CryptifyLOL";
 
 
-function signAndReturn(content){
+async function signAndEncrypt(content){
     // Create a private key
     const { privateKey, publicKey } = crypto.generateKeyPairSync('rsa', {
         modulusLength: 2048,
@@ -12,16 +15,34 @@ function signAndReturn(content){
     const data = Buffer.from(content);
 
     // Sign the data and returned signature in buffer
-    const sign = crypto.sign("SHA256", data , privateKey);
-
-    return (publicKey, sign)
+    const signature = crypto.sign("SHA256", data , privateKey);
 
     // Convert returned buffer to base64
-    const signature = sign.toString('base64');
-    console.log(signature)
-    console.log(crypto.verify("SHA256",data, publicKey, sign))
+    // const signature = sign.toString('base64');
 
-    return (publicKey, sign)
+    // encrypt the data
+    const encrypted = CryptoJS.AES.encrypt(content, secretKey);
+    return {"publicKey": publicKey, "Signature": signature, "encryptedData": encrypted}
+
 }
-signAndReturn("dsfdsfsdfsdfds")
+
+function decryptAndValidate(publicKey, sign, encrypted){
+    const decryptedBytes = CryptoJS.AES.decrypt(encrypted, secretKey)
+    const plaintext = decryptedBytes.toString(CryptoJS.enc.Utf8);
+    const isValid = crypto.verify("SHA256",Buffer.from(plaintext), publicKey, sign)
+
+    return {"data": plaintext, "valid": isValid}
+}
+
+
+signAndEncrypt("dsfdsfsdfsdfdsdfsfweeeeeee").then(
+    (res) => {
+    const {data, isValid} = decryptAndValidate(res.publicKey, res.Signature, res.encryptedData);
+    console.log(data);
+    console.log(isValid);
+    }
+)
+
+
+
 
