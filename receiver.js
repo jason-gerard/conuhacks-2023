b6f698e67cb0c9bbe9bdbe1291a62341518a006b
Jason Gerard <jasongerard321@gmail.com>
2023-01-21 22:52:13 -0500

    Add batching and chunking for large files

--- receiver.js
+++ receiver.js
@@ -4,46 +4,56 @@ const { Client, PrivateKey, AccountCreateTransaction, AccountBalanceQuery, Hbar,
 require("dotenv").config();
 
 async function receive(fileId) {
-    //Grab your Hedera testnet account ID and private key from your .env file
     const myAccountId = process.env.MY_ACCOUNT_ID;
     const myPrivateKey = process.env.MY_PRIVATE_KEY;
 
-    // If we weren't able to grab it, we should throw a new error
     if (!myAccountId || !myPrivateKey) {
         throw new Error("Environment variables MY_ACCOUNT_ID and MY_PRIVATE_KEY must be present");
     }
 
-    // Create our connection to the Hedera network
-    // The Hedera JS SDK makes this really easy!
     const client = Client.forTestnet();
-
     client.setOperator(myAccountId, myPrivateKey);
-
-    //Create new keys
+    
     const newAccountPrivateKey = PrivateKey.generateED25519();
     const newAccountPublicKey = newAccountPrivateKey.publicKey;
 
-    //Create the query
     const query = new FileContentsQuery()
         .setFileId(fileId);
+    const response = (await query.execute(client)).toString();
 
-    //Sign with client operator private key and submit the query to a Hedera network
-    const contents = (await query.execute(client)).toString();
-    
-    const splitIdx = contents.indexOf("\n\n");
-    const fileName = "download-" + contents.substring(0, splitIdx);
-    // Increment the index by 2 to skip both \n chars
-    const fileContent = contents.substring(splitIdx + 2);
-
+    const splitIdx = response.indexOf("\n\n");
+    // Get file name
+    const fileName = "download-" + response.substring(0, splitIdx);
     console.log("Title::\n" + fileName);
-    console.log("contents::\n" + fileContent);
-
-    const fileInfoQuery = new FileInfoQuery()
-        .setFileId(fileId);
-
-    const getInfo = await fileInfoQuery.execute(client);
-    console.log("File info response: " + getInfo.size);
+    // Get list of file ids in order
+    // Increment the index by 2 to skip both \n chars
+    const fileIds = response
+        .substring(splitIdx + 2)
+        .split("\n");
+    console.log("contents::\n" + fileIds);
     
+    const batchContents = [];
+    const batchSize = 50;
+    for (let i = 0; i < fileIds.length / batchSize; i++) {
+        // Promise.all and fetch contents for each file chunk
+        const chunkContent = await Promise.all(
+            fileIds
+                .slice(i * batchSize, (i+1) * batchSize)
+                .map(async (id) => {
+                    const query = new FileContentsQuery()
+                        .setFileId(id);
+
+                    return (await query.execute(client)).toString();
+                })
+        );
+        console.log(chunkContent);
+        batchContents.push(chunkContent);
+    }
+
+    // Concatenate chunks into single file with file name
+    const fileContent = batchContents.flat().join();
+    console.log(fileContent);
+
     return [fileName, fileContent];
 }
 
--- sender.js
+++ sender.js
@@ -18,46 +18,65 @@ async function send(fileName, contents) {
     const newAccountPrivateKey = PrivateKey.generateED25519();
     const newAccountPublicKey = newAccountPrivateKey.publicKey;
 
-    const chunkSize = 1000 * 80;
+    const chunkSize = 1024;
     let startPointer = 0;
     const endPointer = Buffer.byteLength(contents, 'utf8');
     const chunks = [];
 
-    while (startPointer < endPointer){
+    while (startPointer < endPointer) {
         let newStartPointer = startPointer + chunkSize;
         chunks.push(contents.slice(startPointer, newStartPointer));
         startPointer = newStartPointer;
     }
     
+    console.log(`Processing ${chunks.length} chunks`);
+    
+    const start = +new Date();
+    // Create a new file for each chunk and store a list of the file ids in order
+    let fileIds = [];
+    const batchSize = 50;
+    for (let i = 0; i < chunks.length / batchSize; i++) {
+        const batchFileIds = await Promise.all(
+            chunks
+                .slice(i * batchSize, (i+1) * batchSize)
+                .map(async (chunk) => {
+                    const transaction = await new FileCreateTransaction()
+                        .setKeys([newAccountPublicKey])
+                        .setContents(chunk)
+                        .setMaxTransactionFee(new Hbar(2))
+                        .freezeWith(client);
+
+                    const signTx = await transaction.sign(newAccountPrivateKey);
+                    const submitTx = await signTx.execute(client);
+                    const receipt = await submitTx.getReceipt(client);
+                    return receipt.fileId.toString();
+                })
+        );
+        console.log(batchFileIds);
+        fileIds.push(batchFileIds);
+    }
+
+    fileIds = fileIds.flat();
+    console.log(fileIds);
+    
+    const end = +new Date();
+    console.log(`Time elapsed ${(end - start) / 1000} seconds`);
+    
+    // Create manifest file containing the file name and the in order list of file ids
     const fileNameHeader = `${fileName}\n\n`;
-    // Create the transaction file with the file name as the first transaction
-    const fileNameTransaction = await new FileCreateTransaction()
+    const manifestFile = fileNameHeader + fileIds.join("\n");
+    console.log(manifestFile);
+    const manifestFileTransaction = await new FileCreateTransaction()
         .setKeys([newAccountPublicKey])
-        .setContents(fileNameHeader)
+        .setContents(manifestFile)
         .setMaxTransactionFee(new Hbar(2))
         .freezeWith(client);
 
-    const fileNameSignTx = await fileNameTransaction.sign(newAccountPrivateKey);
-    const fileNameSubmitTx = await fileNameSignTx.execute(client);
-    const fileNameTxnReceipt = await fileNameSubmitTx.getReceipt(client);
-
-    console.log("The file ID is: " + fileNameTxnReceipt.fileId);
-    console.log(fileNameTxnReceipt);
-
-    console.log(chunks.length);
-    for (let i = 0; i < chunks.length; i++) {
-        console.log(i);
-        const transaction = await new FileAppendTransaction()
-            .setFileId(fileNameTxnReceipt.fileId)
-            .setContents(chunks[i])
-            .setMaxTransactionFee(new Hbar(2))
-            .freezeWith(client);
-
-        const signTx = await transaction.sign(newAccountPrivateKey);
-        const txResponse = await signTx.execute(client);
-        const receipt = await txResponse.getReceipt(client);
-        console.log(receipt); 
-    }
+    const signTx = await manifestFileTransaction.sign(newAccountPrivateKey);
+    const submitTx = await signTx.execute(client);
+    const receipt = await submitTx.getReceipt(client);
+    
+    console.log(`Manifest file id: ${receipt.fileId.toString()}`);
 }
 
 module.exports = {
